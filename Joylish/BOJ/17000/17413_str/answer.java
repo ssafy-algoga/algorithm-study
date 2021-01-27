9510a7f0fdc07aab2a52aa59e0cbf5bd52a81827
LeeYeonJu <yeonzzu2@gmail.com>
2021-01-27 12:12:51 +0900

    fix : joylish boj 17413 What - 사용하지 않는 코드 삭제(StringTokenizer)

--- Joylish/BOJ/17000/17413_str/answer.java
+++ Joylish/BOJ/17000/17413_str/answer.java
@@ -4,12 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	static BufferedReader br;
-	static StringTokenizer st;
 	static StringBuilder sb;
 	static StringBuilder sbForReversed;
 
