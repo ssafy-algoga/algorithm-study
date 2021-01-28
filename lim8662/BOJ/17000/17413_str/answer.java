f0392742c414bcecf98dad34c7c0acad1f79359b
lim8662 <lim0988@naver.com>
2021-01-28 09:02:16 +0900

    fix : boj 17413 lim8662

--- lim8662/BOJ/17000/17413_str/answer.java
+++ lim8662/BOJ/17000/17413_str/answer.java
@@ -1,33 +1,72 @@
 import java.io.*;
 import java.util.*;
 
-class answer {
-
-/*
-<ab cd>ef gh<ij kl>
-*/
+class boj_17413 {
+	// 단어 뒤집기 2 silver3
 	
+	// baekjoon online judge
+	// <ab cd>ef gh<ij kl>
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		int ans = 0;
+		StringBuilder str = new StringBuilder(br.readLine()); // 입력 문자열 저장
+		StringBuilder ans = new StringBuilder(); // 출력 문자열 저장
+		int cnt = 0;
 		
-		st = new StringTokenizer(br.readLine());
-		String N = st.nextToken();
-		int power = N.length()-1;
+		//태그와 문자열을 분리하여 저장할 배열
+		for (char c : str.toString().toCharArray())
+			if (c == '<')
+				cnt++;
+		if(cnt == 0) cnt = 1; // 태그가 없는 경우 
 		
-		int B = Integer.parseInt(st.nextToken());
-		
-		for(int i = 0; i < N.length(); i++) {
-			char num = N.charAt(i);
-			if(num >= 'A' && num <= 'Z') {
-				ans += Math.pow(B, power--) * (num - 55);
-			} 
+		String[] sub = new String[cnt * 2];
+
+		cnt = 0;
+		// 맨 처음부터 탐색 및 분리
+		while (str.length() > 0) {
+			// 태그면
+			if (str.charAt(0) == '<') {
+				int close = str.indexOf(">");
+
+				// 분리
+				sub[cnt++] = str.substring(0, close + 1);
+				str.delete(0, close + 1);
+			}
+			// 태그가 아니면
 			else {
-				ans += Math.pow(B, power--) * (num - 0);
+				int open = str.indexOf("<");
+				if (open > 0) { // 뒤에 태그가 있으면
+					// 분리
+					sub[cnt++] = str.substring(0, open);
+					str.delete(0, open);
+				
+				} else { // 단어만 있으면
+					sub[cnt++] = str.toString();
+					str.delete(0, str.length());
+				}
 			}
 		}
-		System.out.println(ans);
+
+		for (int i = 0; i < cnt; i++) {
+			// 태그면 append
+			if (sub[i].charAt(0) == '<') {
+				ans.append(sub[i]);
+			}
+			// 단어들이면
+			else {
+				// 단어 분리
+				String[] word = sub[i].split(" ");
+
+				// 뒤집어 append
+				for (String w : word) {
+					StringBuilder rvs = new StringBuilder(w);
+					rvs.reverse();
+					ans.append(rvs).append(" ");
+				}
+				// 마지막 공백 제거
+				ans.deleteCharAt(ans.length() - 1);
+			}
+		}
+		System.out.println(ans.toString());
 	}
-	
+
 }
