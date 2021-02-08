0e983f4c3fc381448e971c7632cfef601f70f48e
newjini <kimyjin0920@gmail.com>
2021-02-08 18:51:06 +0900

    code : 김유진 BOJ 15663 N과M(9)
    코드 수정

--- newjini/BOJ/15000/15663/boj_15663.java
+++ newjini/BOJ/15000/15663/boj_15663.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class boj_15663 {
@@ -12,7 +13,8 @@ public class boj_15663 {
 	static int m;
 	static int[] arr;
 	static StringBuilder sb;
-	static boolean[] visited;
+	static boolean[] isSelected;
+	static LinkedList<Integer> result;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,40 +23,36 @@ public class boj_15663 {
 		m = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine(), " ");
 		arr = new int[n];
+		result = new LinkedList<>();
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(arr); // 입력 받은 배열 sort
 		ArrayList<Integer> result = new ArrayList<>(); // 수열 생성을 위한 배열 
-		int depth = 0; // 재귀 몇 번 돌았는 지 세기 ( m과 비교함 )
-		visited = new boolean[n]; 
+		isSelected = new boolean[n]; 
 		sb = new StringBuilder(); 
-		func(0, depth, result);
+		permu(0);
 		
 	}
 
-	private static void func(int idx, int depth, ArrayList<Integer> result) {
-		if (depth == m) { // depth가 m이면 출력 
-			for (int i : result) { // 5) arr[0] arr[1] 출력
-				sb.append(i+" ");
+	private static void permu(int cnt) {
+		if (cnt == m) { // depth가 m이면 출력 
+			for (int i : result) { 
+				System.out.print(i+" ");
 			}
-			String s = sb.toString();		
-			System.out.println(s);
-			sb.setLength(0);
+			System.out.println();
 			return;
 		}
 		int lastNum = -1; // 중복 방지 변수
 		for (int i = 0; i < n; i++) {
+			if(isSelected[i] || lastNum == arr[i]) continue;
 			
-			if (!visited[i] && lastNum != arr[i]) { // 1) arr[0] 3) arr[1] 7) arr[2] ... 반복
-					
-				visited[i] = true;
-				lastNum = arr[i];
-				result.add(arr[i]); // 2) arr[0] 4) arr[1]
-				func(i, depth + 1, result); // depth 증가
-				result.remove(result.size() - 1); // 6) arr[0] 만 남는다. 
-				visited[i] = false;
-			}
+			result.add(arr[i]);
+			lastNum = arr[i];
+			isSelected[i] = true;
+			permu(cnt+1);
+			isSelected[i] = false;
+			result.remove(result.size()-1);
 		}
 	}
 }
