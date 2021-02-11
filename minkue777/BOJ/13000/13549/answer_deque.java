5cd4bc1a2aa00d376902154aef21bdbb8ce027c2
LeeMingyu <minkue7777@naver.com>
2021-02-12 01:38:21 +0900

    code : boj 13549

---
+++ minkue777/BOJ/13000/13549/answer_deque.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    static final int MAX_SIZE = 100000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int target = sc.nextInt();
+        if(target <= start) {
+            System.out.println(start - target);
+            return;
+        }
+
+        int[] distance = new int[MAX_SIZE+1];
+        Arrays.fill(distance, -1);
+        Deque<Integer> q = new ArrayDeque<>();
+
+        distance[start] = 0;
+        q.offer(start);
+
+        while(!q.isEmpty()) {
+            int curNode = q.poll();
+            if(curNode == target) {
+                System.out.println(distance[curNode]);
+                return;
+            }
+
+            int[] weight = new int[] {0, 1, 1};
+            int[] nextNodes = new int[] {2 * curNode, curNode-1, curNode+1};
+            for(int i=0; i<3; i++) {
+                int nextNode = nextNodes[i];
+                if (nextNode >= 0 && nextNode <= MAX_SIZE && distance[nextNode] == -1) {
+                    if(i == 0) q.addFirst(nextNode);
+                    else q.addLast(nextNode);
+                    distance[nextNode] = distance[curNode] + weight[i];
+                }
+            }
+        }
+    }
+}
